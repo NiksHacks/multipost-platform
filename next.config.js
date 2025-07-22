42108a8cac61628f618083db10b8b0ebe6bd68c2
NiksHacks <tcars1997@gmail.com>
2025-07-22 09:10:41 +0200

    Fix next.config.js - Remove problematic env variables section

--- next.config.js
+++ next.config.js
@@ -3,10 +3,6 @@ const nextConfig = {
   images: {
     domains: ['platform-lookaside.fbsbx.com', 'scontent.cdninstagram.com', 'i.ytimg.com'],
   },
-  env: {
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-  },
 }
 
 module.exports = nextConfig
